5351261d94507a13c46b3caa1fc5280fba754555
MangoICT <service@mangoict.com>
2015-05-14 16:38:56 +0200

    Made the boilerplate really easy!

--- src/website/gwy/sys/db/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$CFG = array(
-    "host" => "localhost", //"phpmyadmin30.sohosted.com:3351", //"localhost:3351",
-    "usr" => "mangoict_db2", //"driftbandendb1",
-    "pwd" => "mn*09ag3673a",
-    "db" => "mangoict_db2", //"driftbanden_db1"
-);
